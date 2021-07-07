007940dfa905112063dadb0e70e774666565a03c
Joshua Behrens <behrens@heptacom.de>
2021-07-07 22:08:32 +0200

    Add preview configuration

--- src/Portal.php
+++ src/Portal.php
@@ -8,11 +8,22 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class Portal extends PortalContract
 {
+    public const CONFIG_API_KEY = 'api_key';
+
+    public const CONFIG_PREVIEW = 'preview';
+
+    public const CONFIG_PREVIEW_LIMIT = 'preview_limit';
+
     public function getConfigurationTemplate(): OptionsResolver
     {
-        return parent::getConfigurationTemplate()
-            ->setRequired(['api_key'])
-            ->setAllowedTypes('api_key', 'string')
-            ->setDefault('api_key', 'DEMO_KEY');
+        return parent::getConfigurationTemplate()->setDefaults([
+            self::CONFIG_PREVIEW => true,
+            self::CONFIG_PREVIEW_LIMIT => 10,
+        ])
+            ->setAllowedTypes(self::CONFIG_PREVIEW, 'bool')
+            ->setAllowedTypes(self::CONFIG_PREVIEW_LIMIT, 'int')
+            ->setRequired([self::CONFIG_API_KEY])
+            ->setAllowedTypes(self::CONFIG_API_KEY, 'string')
+            ->setDefault(self::CONFIG_API_KEY, 'DEMO_KEY');
     }
 }
--- src/Resources/flow-component/media.php
+++ src/Resources/flow-component/media.php
@@ -4,18 +4,42 @@ declare(strict_types=1);
 use Heptacom\HeptaConnect\Dataset\Base\Date;
 use Heptacom\HeptaConnect\Dataset\Ecommerce\Media\Media;
 use Heptacom\HeptaConnect\Portal\Base\Builder\FlowComponent;
+use Heptacom\HeptaConnect\Portal\Base\Portal\Contract\ConfigurationContract;
 use NiemandOnline\HeptaConnect\Portal\NasaApod\Packer\MediaPacker;
+use NiemandOnline\HeptaConnect\Portal\NasaApod\Portal;
 use NiemandOnline\HeptaConnect\Portal\NasaApod\Support\NasaApodClient;
+use Psr\Log\LoggerInterface;
 
 FlowComponent::explorer(Media::class)
-    ->run(static function(NasaApodClient $api, MediaPacker $packer): iterable {
+    ->run(static function(NasaApodClient $api, MediaPacker $packer, LoggerInterface $logger, ConfigurationContract $config): iterable {
         $now = new Date();
         $oneDayInterval = new \DateInterval('P1D');
+        $counter = null;
+
+        if ($config->get(Portal::CONFIG_PREVIEW)) {
+            $counter = (int) $config->get(Portal::CONFIG_PREVIEW_LIMIT);
+        }
 
         while ($now->format('Y-m-d') >= '1995-06-16') {
             try {
-                yield $packer->pack($api->getImageOfTheDay($now));
+                $apodEntry = $api->getImageOfTheDay($now);
+
+                if ($apodEntry === null) {
+                    $logger->info('End reached. (Might be due to API limit)');
+                    break;
+                }
+
+                yield $packer->pack($apodEntry);
+
+                if ($counter !== null && --$counter === 0) {
+                    break;
+                }
             } catch (\RuntimeException $exception) {
+            } catch (\Throwable $exception) {
+                $logger->warning('End reached due to: ' . $exception->getMessage(), [
+                    'exception' => $exception,
+                ]);
+                break;
             }
 
             $now = $now->sub($oneDayInterval);
